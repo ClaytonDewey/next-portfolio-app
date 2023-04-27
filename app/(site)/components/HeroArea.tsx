9e80adf2df7db7e9cda0eba16d370badd4a98d65
ClaytonDewey <clay@dryadmedia.com>
2023-04-27 17:42:41 -0500

    Style HeroArea

--- app/(site)/components/HeroArea.tsx
+++ app/(site)/components/HeroArea.tsx
@@ -1,21 +1,33 @@
 import Image from 'next/image';
 import React from 'react';
 import profilePic from '../../assets/profile.jpeg';
+import Nav from './Nav';
 
 type HeroAreaProps = {};
 
 const HeroArea: React.FC<HeroAreaProps> = () => {
   return (
-    <section>
-      <Image src={profilePic} alt='Clayton Dewey' />
-      <h3>Hi, I&apos;m Clayton!</h3>
-      <p>Front End Developer</p>
+    <div id='home'>
+      <Nav />
+      <div className='header__index'>
+        <div className='header__wrapper'>
+          <div className='page-header'>
+            <Image
+              src={profilePic}
+              alt='Clayton Dewey'
+              className='header__image-profile'
+            />
+            <h1>Hi, I&apos;m Clayton!</h1>
+            <p className='subhead'>Front End Developer</p>
 
-      <div>
-        <button>My Portfolio</button>
-        <button>Hire Me</button>
+            <div>
+              <button>My Portfolio</button>
+              <button>Hire Me</button>
+            </div>
+          </div>
+        </div>
       </div>
-    </section>
+    </div>
   );
 };
 export default HeroArea;
