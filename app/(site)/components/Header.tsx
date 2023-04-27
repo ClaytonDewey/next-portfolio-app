127602ed875a3f69a8f982970e66bbb3c5a175d8
ClaytonDewey <clay@dryadmedia.com>
2023-04-27 14:57:12 -0500

    Add navbar

--- app/(site)/components/Header.tsx
+++ app/(site)/components/Header.tsx
@@ -1,18 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { navbar } from '../content/navbar';
+import Link from 'next/link';
 
 type HeaderProps = {};
 
 const Header: React.FC<HeaderProps> = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  const [scrollNav, setScrollNav] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      setScrollNav(window.scrollY > 50);
+    });
+  }, []);
+
   return (
-    <header>
-      <h1>Clayton Dewey</h1>
-      <div className='navigation'>
-        {navbar.map((item, index) => {
-          return <p key={index}>{item.title}</p>;
-        })}
-      </div>
-    </header>
+    <>
+      <nav
+        className={`nav__primary ${isNavOpen ? 'open' : ''} ${
+          scrollNav ? 'bg-dark' : ''
+        }`}>
+        <button
+          onClick={() => setIsNavOpen(!isNavOpen)}
+          className='nav__toggler'>
+          <span className='icon__bar top__bar'></span>
+          <span className='icon__bar top__bar'></span>
+          <span className='icon__bar top__bar'></span>
+          <span className='visually-hidden'>Toggle navigation</span>
+        </button>
+
+        <ul>
+          {navbar.map((item, index) => {
+            return (
+              <li key={index}>
+                <Link href={`#${item.title.toLowerCase()}`}>{item.title}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </>
   );
 };
 export default Header;
