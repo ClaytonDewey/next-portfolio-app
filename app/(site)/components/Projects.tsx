9f403d43894b031127700f8914910e2852b0b231
ClaytonDewey <clay@dryadmedia.com>
2023-04-27 13:57:35 -0500

    Create Projects/Portfolio component

---
+++ app/(site)/components/Projects.tsx
@@ -0,0 +1,26 @@
+import { getProjects } from '@/sanity/sanity-utils';
+import Image from 'next/image';
+
+const Projects = async () => {
+  const projects = await getProjects();
+
+  return (
+    <div>
+      <h2>My Projects</h2>
+      {projects.map((project) => (
+        <div key={project._id}>
+          {project.image && (
+            <Image
+              src={project.image}
+              alt={project.name}
+              width={750}
+              height={400}
+            />
+          )}
+          <p>{project.name}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+export default Projects;
