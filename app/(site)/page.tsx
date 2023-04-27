509bc38620e27cc998845a2674f28a5d3ef7c890
ClaytonDewey <clay@dryadmedia.com>
2023-04-27 13:57:45 -0500

    Import components

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,26 +1,20 @@
 import { getProjects } from '@/sanity/sanity-utils';
-import Image from 'next/image';
+import Header from './components/Header';
+import Projects from './components/Projects';
+import HeroArea from './components/HeroArea';
 
 const Home = async () => {
   const projects = await getProjects();
 
   return (
-    <div>
-      <h2>My Projects</h2>
-      {projects.map((project) => (
-        <div key={project._id}>
-          {project.image && (
-            <Image
-              src={project.image}
-              alt={project.name}
-              width={750}
-              height={400}
-            />
-          )}
-          <p>{project.name}</p>
-        </div>
-      ))}
-    </div>
+    <>
+      <Header />
+      <HeroArea />
+      <main>
+        {/* @ts-expect-error Server Component */}
+        <Projects />
+      </main>
+    </>
   );
 };
 export default Home;
