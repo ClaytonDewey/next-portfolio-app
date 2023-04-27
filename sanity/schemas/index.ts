79bcc9e2ea601ea7bd0ba2d90c76abfdada531eb
ClaytonDewey <clay@dryadmedia.com>
2023-04-27 12:45:54 -0500

    Create project & tool schemas

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,5 +1,7 @@
+import project from './project-schema';
+import tool from './tool-schema';
 import page from './page-schema';
 
-const schemas = [page];
+const schemas = [project, tool, page];
 
 export default schemas;
---
+++ sanity/schemas/tool-schema.ts
@@ -0,0 +1,8 @@
+const tool = {
+  name: 'tool',
+  title: 'Tools',
+  type: 'document',
+  fields: [{ name: 'name', title: 'Tool', type: 'string' }],
+};
+
+export default tool;
