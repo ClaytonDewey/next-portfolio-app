74e193fc788ef7a3496459c227fed3ec921b28af
ClaytonDewey <clay@dryadmedia.com>
2023-04-28 15:12:38 -0500

    Update portfolio dispay

--- app/(site)/components/Projects.tsx
+++ app/(site)/components/Projects.tsx
@@ -5,22 +5,35 @@ const Projects = async () => {
   const projects = await getProjects();
 
   return (
-    <div>
-      <h2>My Projects</h2>
-      {projects.map((project) => (
-        <div key={project._id}>
-          {project.image && (
-            <Image
-              src={project.image}
-              alt={project.name}
-              width={750}
-              height={400}
-            />
-          )}
-          <p>{project.name}</p>
-        </div>
-      ))}
-    </div>
+    <section id='portfolio'>
+      <h2 className='section__title'>
+        Portfolio
+        <br />
+        <span>My Work</span>
+      </h2>
+      <div className='flex flex__row-wrap'>
+        {projects.map((project) => (
+          <div key={project._id} className='project'>
+            <div className='project__body'>
+              <div className='project__image'>
+                {project.image && (
+                  <Image
+                    src={project.image}
+                    alt={project.name}
+                    width={750}
+                    height={400}
+                  />
+                )}
+              </div>
+              <div className='project__header'>
+                <h2 className='project__title'>{project.name}</h2>
+                <h3 className='project__type'>{project.type}</h3>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
   );
 };
 export default Projects;
